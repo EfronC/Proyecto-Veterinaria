a463d5a43d70f29e8889999258663e59e554dbf7
Efrain <efrondx@gmail.com>
2014-11-24 11:00:00 -0600

    Proyecto de Veterinarias

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name proyectoVeterinariaApp
+ * @description
+ * # proyectoVeterinariaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('proyectoVeterinariaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectoVeterinariaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proyectoVeterinariaApp
+ */
+angular.module('proyectoVeterinariaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
